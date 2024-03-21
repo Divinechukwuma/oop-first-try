c3468aba6646da22349b9feea7ea676d64157359
divinechukwuma <divinechukwuma982@gmail.com>
2024-03-21 09:23:33 +0100

    time for the oop

--- index.php
+++ index.php
@@ -7,7 +7,23 @@
 </head>
 <body>
 
+<form action="" method="POST" class="parent">
+    <fieldset><h1>Sign up </h1>
+    <label for="name"> Name</label>
+    <input type="text" placeholder="Yourname" name="name">
+    <label for="Password">Password</label>
+    <input type="text" placeholder="Your password" name="password">
+    <label for="Confirm-Password">Confirm Password</label>
+    <input type="text" placeholder="Confirm password" name="confirm-password">
+    <a href="#">Login</a>
+    <a href="#">Sign Up</a>
+    <button name="submit">submit</button>
+</fieldset>
+</form>
 
+<?php
+
+?>
     
 </body>
 </html>
